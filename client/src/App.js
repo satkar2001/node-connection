2f9c0c8ec0f32817de92af6cd9fc3cb10cc2986b
satkar2001 <satkaracharya883@gmail.com>
2021-08-01 22:16:52 +0530

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios';
 function App() {
+  function getdatafromNode(){
+      axios.get('/services')
+       .then(res=>console.log(res))
+       .catch(err=>console.log(err))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hfiifhos</h1>
+      <button onClick={getdatafromNode}>get data from back</button>
     </div>
   );
 }
